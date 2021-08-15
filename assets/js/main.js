5a6959d853004fdc869bb1aaf4683a1f09084f8f
cerveauplus <digitalsouag@gmail.com>
2021-08-15 13:10:23 +0100

    updated aleatoire images and add garba section

--- assets/js/main.js
+++ assets/js/main.js
@@ -18,3 +18,19 @@
     });
 
 })(jQuery);
+// Carousel garba
+let items = document.querySelectorAll('.carousel .carousel-item')
+
+items.forEach((el) => {
+    const minPerSlide = 5
+    let next = el.nextElementSibling
+    for (var i = 1; i < minPerSlide; i++) {
+        if (!next) {
+            // wrap carousel by using first child
+            next = items[0]
+        }
+        let cloneChild = next.cloneNode(true)
+        el.appendChild(cloneChild.children[0])
+        next = next.nextElementSibling
+    }
+})
